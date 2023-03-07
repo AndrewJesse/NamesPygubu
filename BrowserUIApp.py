38c36083c1ea4602a11e34f23d962e5c3d8a0da4
A.J. Marchese <aj.marchese@outlook.com>
2023-03-07 08:09:49 -0800

    Added database and name classes

--- BrowserUIApp.py
+++ BrowserUIApp.py
@@ -10,6 +10,7 @@ class BrowserApp:
     def __init__(self, parent):
         self.start_ui(parent)
 
+
     def start_ui(self, parent):
         builder = pygubu.Builder()
         builder.add_resource_path(PROJECT_PATH)
@@ -18,10 +19,15 @@ class BrowserApp:
         self.mainwindow = builder.get_object("top_level", parent)
         builder.connect_callbacks(self)
 
+        # retrieves the radio button widget object using the get_object() method of the Builder() object and assigns it to an instance variable for later use in callbacks.
         self.builder = builder
         self.male_button = builder.get_object('male_radio', parent)
         self.female_button = builder.get_object('female_radio', parent)
         self.name_entry = builder.get_object('name_entry', parent)
+
+    def _search_names(self):
+        pass
+
 
 
 
